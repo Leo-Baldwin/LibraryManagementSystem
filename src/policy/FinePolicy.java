40d0aa37b10187a499ff7d9ecc3cf28b5ab78d16
leobaldwin <leobaldwin1@icloud.com>
2025-10-30 15:14:41 +0000

    Add implementation for policy interfaces and classes

--- src/policy/FinePolicy.java
+++ src/policy/FinePolicy.java
@@ -1,4 +1,19 @@
 package policy;
 
+import java.time.LocalDate;
+
+/**
+ * Interface class for calculating overdue fines.
+ */
 public interface FinePolicy {
+
+    /**
+     * Calculates the fine in pence for an item returned on {@code returnDate}
+     * when the due date was {@code dueDate}.
+     *
+     * @param dueDate the due date of the loan
+     * @param returnDate the actual return date of the loan
+     * @return non-negative fine amount in pence
+     */
+    int calculateFine(LocalDate dueDate, LocalDate returnDate);
 }
--- src/policy/StandardLoanPolicy.java
+++ src/policy/StandardLoanPolicy.java
@@ -1,4 +1,35 @@
 package policy;
 
-public class StandardLoanPolicy {
+import java.time.LocalDate;
+
+public class StandardLoanPolicy implements LoanPolicy {
+
+    /** Number of days to add to the loan starting date to get the due date. */
+    private final int loanDays;
+
+    /**
+     * Creates a policy with the specified loan period
+     *
+     * @param loanDays number of days for the loan period
+     */
+    public StandardLoanPolicy(int loanDays) {
+        this.loanDays = loanDays;
+    }
+
+    /** @return the loan period in days
+     */
+    public int getLoanDays() {
+        return loanDays;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public LocalDate calculateDueDate(LocalDate loanDate) {
+        if (loanDate == null) {
+            throw new IllegalArgumentException("loanDate cannot be null");
+        }
+        return loanDate.plusDays(loanDays);
+    }
 }
